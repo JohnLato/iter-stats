71cdd55a83b4faece9c8de05e9febf5afae3e1d2
John Lato <jwlato@tsurucapital.com>
2014-05-19 15:56:01 +0900

    tests: more robust equality tests

--- tests/Statistics/Iteratee/Tests.hs
+++ tests/Statistics/Iteratee/Tests.hs
@@ -15,13 +15,29 @@ tests =
   [ testGroup "Sample" $ map mkUnProp uns
   ]
 
-unsProp :: (Eq a)
+-- Don't want to drag in more dependencies for just this.
+class ApproxEq a where
+    approxEq :: Double -> a -> a -> Bool
+
+instance ApproxEq Double where
+    approxEq tol d1 d2 = m == 0.0 || d/m < tol where
+      m = max (abs d1) (abs d2)
+      d = abs (d1 - d2)
+
+instance (ApproxEq a, ApproxEq b) => ApproxEq (a,b) where
+    approxEq tol (l1,r1) (l2,r2) = approxEq tol l1 l2 && approxEq tol r1 r2
+
+infix 4 ===
+(===) :: (ApproxEq a) => a -> a -> Bool
+(===) = approxEq {-pretty equal-}1.0e-10
+
+unsProp :: (ApproxEq a)
         => (V.Vector Double -> a)
         -> (Iteratee [Double] Identity a)
         -> [Double]
         -> Bool
 unsProp vec iter xs = if null xs then True
-    else vec (V.fromList xs) == (runIdentity $ run =<< enumPure1Chunk xs iter)
+    else vec (V.fromList xs) === (runIdentity $ run =<< enumPure1Chunk xs iter)
     -- we're using Eq for doubles, which is always a bad idea...
 
     -- also not checking empty vectors, because in some cases (range,
